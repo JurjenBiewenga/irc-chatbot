f9f8a139c1a63f362e2ff53b7879923f88ab5ff3
Jurjen Biewenga <JurjenBiewenga@users.noreply.github.com>
2016-06-22 15:14:53 +0200

    Update Insult.cs

--- src/modules/Insult.cs
+++ src/modules/Insult.cs
@@ -1,10 +1,10 @@
 
 /**
  *  @project >> Internet Relay Chat: Chatbot
- *  @authors >> Adib
- *  @version >> 02.00.00
- *  @release >> 06.06.16
- *  @licence >> MIT
+ *  @authors >> Adib, SaraDR
+ *  @version >> 02.01.00
+ *  @updated >> 22.06.16
+ *  @licence >> QPL-1.0
  */
 
 using System;
@@ -17,63 +17,80 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Web.Script.Serialization;
 
 namespace IRC
 {
-    public static class Insult
+    public class Insult : Module
     {
-
         #region
-        static JavaScriptSerializer serializer = new JavaScriptSerializer();
-        static HttpWebRequest request;
-
-        /**
-         *  This event is called whenever someone, either in a private query
-         *  or in a channel, calls up the bot, with any command that matches
-         *  this modules synonyms. Any remaining part of the message is then
-         *  delivered here along with the room to reply to, and the user who
-         *  called it. In order to send out a message back out, then use the
-         *  syntax of 'Anxious.Send(<target>, <message>)'. If the message is
-         *  supposed to be send in private then insert the user parameter in
-         *  the target, else insert the rooms parameter. In the message part
-         *  please keep the replies short to avoid sending too much flooding
-         */
-        public static async void OnMessage(string user, string room, string text)
-        {
-            try
+            
+            public override string pattern ()
             {
-
-                request = (HttpWebRequest)WebRequest.Create("http://quandyfactory.com/insult/json");
-                request.AllowAutoRedirect = false;
-
-                request.BeginGetResponse(new AsyncCallback(AsyncResponse), null);
-
-
+                return ("^!(?:insult|abuse|disrespect|taunt)\\s+(\\S+)$");
             }
-            catch
+            
+        #endregion
+        
+        #region
+            
+            /**
+             *  This event is called whenever someone, either in a private query
+             *  or in a channel, calls up the bot, with any command that matches
+             *  this modules synonyms. Any remaining part of the message is then
+             *  delivered here along with the room to reply to, and the user who
+             *  called it. In order to send out a message back out, then use the
+             *  syntax of 'Anxious.Send(<target>, <message>)'. If the message is
+             *  supposed to be send in private then insert the user parameter in
+             *  the target, else insert the rooms parameter. In the message part
+             *  please keep the replies short to avoid sending too much flooding
+             */
+            public override void OnChannelMessage (string user, string room, string text)
             {
-
+                Console.WriteLine("A");
+                
+                try
+                {
+                    request = (HttpWebRequest)WebRequest.Create("http://quandyfactory.com/insult/json");
+                    
+                    request.AllowAutoRedirect = false;
+                    
+                    request.BeginGetResponse(new AsyncCallback(AsyncResponse), new object[3] {user, room, text});
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
-        }
-
-        private static void AsyncResponse(IAsyncResult asynchronousResult)
-        {
-            HttpWebRequest myHttpWebRequest = request;
-            var response = (HttpWebResponse)myHttpWebRequest.EndGetResponse(asynchronousResult);
-
-
-            var headers = response.Headers;
-
-            using (var reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII))
+            
+        #endregion
+        
+        #region
+            
+            public void AsyncResponse (IAsyncResult result)
             {
-                var responseText = reader.ReadToEnd();
-                var responseObject = serializer.Deserialize<Dictionary<string, string>>(responseText);
-
-                var outputText = responseObject["insult"];
-                Anxious.Send(room, outputText + ", " + Title.GetUser(text));
+                object[] parameters = (object[])result.AsyncState;
+                
+                var myHttpWebRequest = request;
+                
+                using (var response = (HttpWebResponse)myHttpWebRequest.EndGetResponse(result))
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII))
+                    {
+                        var outputText = serializer.Deserialize<Dictionary<string, string>>(reader.ReadToEnd())["insult"];
+                        
+                        if (outputText.Length > 0)
+                        {
+                            Anxious.Send((string)parameters[1], (string)parameters[2] + ", " + outputText);
+                        }
+                    }
+                }
             }
-            response.Close();
-        }
+            
+            public HttpWebRequest       request     = null;
+            
+            public JavaScriptSerializer serializer  = new JavaScriptSerializer();
+            
         #endregion
     }
 }
